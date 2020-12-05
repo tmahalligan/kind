f9618b6faac57b0a5dbeb590dcd1291b4e65bfb4
Thomas Halligan <tmhalligan@gmail.com>
2020-12-05 18:45:01 -0500

    Adjusting automation for nginx-ingress

--- automation/registry.sh
+++ automation/registry.sh
@@ -30,9 +30,11 @@ nodes:
   extraPortMappings:
   - containerPort: 80
     hostPort: 80
+    listenAddress: 127.0.0.1  # omit for 0.0.0.0
     protocol: TCP
   - containerPort: 443
     hostPort: 443
+    listenAddress: 127.0.0.1  # omit for 0.0.0.0
     protocol: TCP
 EOF
 
